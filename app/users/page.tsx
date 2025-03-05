3002f06bb14306409a6d2c584ba18e643574ee3b
Scott Zhang <sandorzhang@gmail.com>
2025-03-06 00:32:44 +0800

    清理

--- app/users/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserList } from '@/components/UserList';
-
-export default function UsersPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <UserList />
-    </div>
-  );
-} 
